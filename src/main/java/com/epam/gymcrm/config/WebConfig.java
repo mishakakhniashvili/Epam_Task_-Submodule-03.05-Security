b9e876d738f49d62fbb760445140c5ceb52d36d0
Mikheili Kakhniashvili <mikheili_kakhniashvili@epam.com>
2026-06-16 05:19:52 +0400

    Add transaction id logging

--- src/main/java/com/epam/gymcrm/config/WebConfig.java
+++ src/main/java/com/epam/gymcrm/config/WebConfig.java
@@ -1,4 +1,28 @@
 package com.epam.gymcrm.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.epam.gymcrm")
+@PropertySource("classpath:application.properties")
+@EnableTransactionManagement
+public class WebConfig implements WebMvcConfigurer {
+
+    private final TransactionIdInterceptor transactionIdInterceptor;
+
+    public WebConfig(TransactionIdInterceptor transactionIdInterceptor) {
+        this.transactionIdInterceptor = transactionIdInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(transactionIdInterceptor);
+    }
 }
